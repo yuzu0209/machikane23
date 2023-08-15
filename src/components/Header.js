78d99b481bfaeb64de963070b9b1f7e0be5a7776
Maltonn <ymatsubayashi09@outlook.jp>
2023-08-15 16:39:38 +0900

    ハンバーガーメニューを追加

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,27 @@
+import { useState } from "react"
+
 import "../css/Header.css"
+import hamburger from "../imgs/hamburger.svg"
+
 export default function Header() {
+    const [isHamburgerOpen, setIsHamburgerOpen] = useState(false)
+    
+    function onHamburgerClick() {
+        setIsHamburgerOpen(prev => !prev)
+    }
+
     return (
         <header>
             <h1>まちかね祭2023 ヘッダー</h1>
+
+            <div className={`menu ${isHamburgerOpen?"menu-opend":"menu-closed"}`}>
+                <div>
+                    ここにメニュー
+                </div>
+            </div>
+            <div className="hamburger" onClick={onHamburgerClick}>
+                <img src={hamburger}></img>
+            </div>
         </header>
     )
 }
