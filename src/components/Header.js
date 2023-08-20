f0a1fb3efe323bf563dfee682022c461846e59b5
Sato-Yoshiki <u979628a@ecs.osaka-u.ac.jp>
2023-08-21 00:17:13 +0900

    humhum

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,9 +10,21 @@ export default function Header() {
     function onHamburgerClick() { //ハンバーガーがクリックされた時の処理
         setIsHamburgerOpen(prev => !prev)
     }
-
-    function menuOpen(){
-        document.querySelectorAll('A')
+    const [isMenuOpenA, setIsMenuOpenA] = useState(false)
+    function menuOpenA(){
+        setIsMenuOpenA(prev => !prev)
+    }
+    const [isMenuOpenB, setIsMenuOpenB] = useState(false)
+    function menuOpenB(){
+        setIsMenuOpenB(prev => !prev)
+    }
+    const [isMenuOpenC, setIsMenuOpenC] = useState(false)
+    function menuOpenC(){
+        setIsMenuOpenC(prev => !prev)
+    }
+    const [isMenuOpenD, setIsMenuOpenD] = useState(false)
+    function menuOpenD(){
+        setIsMenuOpenD(prev => !prev)
     }
     return (
         <header>
@@ -21,37 +33,37 @@ export default function Header() {
             <div className={`menu ${isHamburgerOpen?"menu-opend":"menu-closed"}`}>
                 <div className="ja-menu">
                     <ul>
-                        <li><span className="menu-parent A" onClick={menuOpen}>まちかね祭について</span>
-                            <ul>
-                                <li className="menu-child A"><a href="/23/about">まちかね祭とは</a></li>
-                                <li className="menu-child A"><a href="/23/thema">テーマ</a></li>
-                                <li className="menu-child A"><a href="/23/">グッズ</a></li>
-                                <li className="menu-child A"><a href="/23/">ご挨拶</a></li>
-                                <li className="menu-child A"><a href="/23/">ご協賛</a></li>
+                        <li><span className={`menu-parent ${isMenuOpenA?"is-opend":"is-closed"}`} onClick={menuOpenA}>まちかね祭について</span>
+                            <ul className={`menu-child ${isMenuOpenA?"is-opend":"is-closed"}`}>
+                                <li><a href="/23/about">まちかね祭とは</a></li>
+                                <li><a href="/23/thema">テーマ</a></li>
+                                <li><a href="/23/">グッズ</a></li>
+                                <li><a href="/23/">ご挨拶</a></li>
+                                <li><a href="/23/">ご協賛</a></li>
                             </ul>
                         </li>
-                        <li><span className="menu-parent B" onClick={menuOpen}>企画について</span>
-                            <ul>
-                                <li className="menu-child B"><a href="/23/">企画区分について</a></li>
-                                <li className="menu-child B"><a href="/23/project">企画検索</a></li>
-                                <li className="menu-child B"><a href="/23/">ステージタイムテーブル</a></li>
-                                <li className="menu-child B"><a href="/23/">ラリー企画一覧</a></li>
-                                <li className="menu-child B"><a href="/23/">委員会企画</a></li>
-                                <li className="menu-child B"><a href="/23/">研究室紹介</a></li>
+                        <li><span className={`menu-parent ${isMenuOpenB?"is-opend":"is-closed"}`} onClick={menuOpenB}>企画について</span>
+                            <ul className={`menu-child ${isMenuOpenB?"is-opend":"is-closed"}`}>
+                                <li><a href="/23/">企画区分について</a></li>
+                                <li><a href="/23/project">企画検索</a></li>
+                                <li><a href="/23/">ステージタイムテーブル</a></li>
+                                <li><a href="/23/">ラリー企画一覧</a></li>
+                                <li><a href="/23/">委員会企画</a></li>
+                                <li><a href="/23/">研究室紹介</a></li>
                             </ul>
                         </li>
-                        <li><span className="menu-parent C" onClick={menuOpen}>来場者の方へ</span>
-                            <ul>
-                                <li className="menu-child C"><a href="/23/">注意事項</a></li>
-                                <li className="menu-child C"><a href="/23/">公式パンフレット</a></li>
-                                <li className="menu-child C"><a href="/23/">よくあるご質問</a></li>
-                                <li className="menu-child"><a href="/23/">お問い合わせ</a></li>
+                        <li><span className={`menu-parent ${isMenuOpenC?"is-opend":"is-closed"}`} onClick={menuOpenC}>来場者の方へ</span>
+                            <ul className={`menu-child ${isMenuOpenC?"is-opend":"is-closed"}`}>
+                                <li><a href="/23/">注意事項</a></li>
+                                <li><a href="/23/">公式パンフレット</a></li>
+                                <li><a href="/23/">よくあるご質問</a></li>
+                                <li><a href="/23/">お問い合わせ</a></li>
                             </ul>
                         </li>
-                        <li><span className="menu-parent D" onClick={menuOpen}>アクセス・マップ</span>
-                            <ul>
-                                <li className="menu-child D"><a href="/23/">アクセス</a></li>
-                                <li className="menu-child D"><a href="/23/">マップ</a></li>
+                        <li><span className={`menu-parent ${isMenuOpenD?"is-opend":"is-closed"}`} onClick={menuOpenD}>アクセス・マップ</span>
+                            <ul className={`menu-child ${isMenuOpenD?"is-opend":"is-closed"}`}>
+                                <li><a href="/23/">アクセス</a></li>
+                                <li><a href="/23/">マップ</a></li>
                             </ul>
                         </li>
                     </ul>
